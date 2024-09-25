5f8c95b791d03f55b6c3a88a549011137a1fee13
jai-singh5 <jaisingh09520@gmail.com>
2024-09-25 23:10:59 +0530

    added more projects and changes in contact

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -6,17 +6,19 @@ function Projects() {
     <>
       <div>
         <div className="projects">
-          <a href="https://github.com/jai-singh5/Spotify-Clone.git"><div className="card">
-            <img src="spotify-clone.png" alt="" />
-            <h2>Spotify Clone</h2>
-            <p>Made a Project on music streaming application built using JavaScript, HTML, and CSS. It features a sleek, user-friendly interface that mimics the look and feel of Spotify. Users can browse and play tracks, view playlists, and enjoy a seamless listening experience.</p>
-          </div> </a>
+          <a href="https://github.com/jai-singh5/Spotify-Clone.git">
+            <div className="card">
+              <img src="spotify-clone.png" alt="" />
+              <h2>Spotify Clone</h2>
+              <p>Made a Project on music streaming application built using JavaScript, HTML, and CSS. It features a sleek, user-friendly interface that mimics the look and feel of Spotify. Users can browse and play tracks, view playlists, and enjoy a seamless listening experience.</p>
+            </div> </a>
           <hr />
-          <a href="https://github.com/jai-singh5/To-Do-List.git"><div className="card">
-            <img src="To-Do List.png" alt="" />
-            <h2>To-Do List</h2>
-            <p>This project is a simple yet functional To-Do List application developed using JavaScript, HTML, and CSS. It allows users to add, edit, and remove tasks with ease.The simple interface and responsive design ensure a smooth user experience.</p>
-          </div></a>
+          <a href="https://github.com/jai-singh5/To-Do-List.git">
+            <div className="card">
+              <img src="To-Do List.png" alt="" />
+              <h2>To-Do List</h2>
+              <p>This project is a simple yet functional To-Do List application developed using JavaScript, HTML, and CSS. It allows users to add, edit, and remove tasks with ease.The simple interface and responsive design ensure a smooth user experience.</p>
+            </div></a>
           <hr />
           <a href="https://github.com/jai-singh5/Snake-Game.git"><div className="card">
             <img src="Snake Game.png" alt="" />
@@ -24,16 +26,27 @@ function Projects() {
             <p>This classic Snake Game is built with JavaScript, HTML, and CSS. It features smooth gameplay and a responsive design, offering a nostalgic gaming experience. Players navigate the snake to collect food while avoiding collisions with the walls and the snake's own tail.</p>
           </div></a>
           <hr />
-          <a href="https://github.com/jai-singh5/Portfolio.git"><div className="card">
-            <img src="Portfolio.png" alt="" />
-            <h2>Portfolio</h2>
-            <p>This portfolio website, built with React.js, showcases my projects, skills in a sleek and interactive format. With responsive design and layout, it highlights my expertise in web development. This site demonstrates my ability to create dynamic and professional web applications.</p>
-          </div></a>
-          <a href="https://github.com/jai-singh5/Fuel-My-Work.git"><div className="card">
-            <img src="Fuel My Work!.png" alt="" />
-            <h2>Fuel My Work!</h2>
-            <p>Fuel My Work is a crowdfunding platform built with Next.js and Tailwind CSS, designed for fans to financially support their favorite creators' projects. It empowers creators—developers, artists, and influencers—to bring their ideas to life with the backing of their community.</p>
-          </div></a>
+          <a href="https://github.com/jai-singh5/Portfolio.git">
+            <div className="card">
+              <img src="Portfolio.png" alt="" />
+              <h2>Portfolio</h2>
+              <p>This portfolio website, built with React.js, showcases my projects, skills in a sleek and interactive format. With responsive design and layout, it highlights my expertise in web development. This site demonstrates my ability to create dynamic and professional web applications.</p>
+            </div></a>
+            <hr />
+          <a href="https://github.com/jai-singh5/Fuel-My-Work.git">
+            <div className="card">
+              <img src="Fuel My Work!.png" alt="" />
+              <h2>Fuel My Work!</h2>
+              <p>Fuel My Work is a crowdfunding platform built with Next.js and Tailwind CSS, designed for fans to financially support their favorite creators' projects. It empowers creators—developers, artists, and influencers—to bring their ideas to life with the backing of their community.</p>
+            </div></a>
+            <hr />
+          <a href="https://github.com/jai-singh5/X-Twitter-.git">
+            <div className="card">
+              <img src="X.png" alt="" />
+              <h2>X(Twitter) - Clone</h2>
+              <p>This project is a simple clone of the X (formerly Twitter) UI using only HTML and Tailwind CSS. The goal is to replicate the main user interface of X while leveraging Tailwind CSS for utility-first styling. Features</p>
+            </div></a>
+            <hr />
         </div>
       </div>
     </>
