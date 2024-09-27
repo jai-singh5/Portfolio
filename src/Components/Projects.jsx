acc0cac5c0ea04ccf2498027d05c2b3d71bc9168
jai-singh5 <jaisingh09520@gmail.com>
2024-09-27 20:56:25 +0530

    added 3 more projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -6,12 +6,54 @@ function Projects() {
     <>
       <div>
         <div className="projects">
+          <a href="https://github.com/jai-singh5/Fuel-My-Work.git">
+            <div className="card">
+              <img src="Fuel My Work!.png" alt="" />
+              <h2>Fuel My Work!</h2>
+              <p>Fuel My Work is a crowdfunding platform built with Next.js and Tailwind CSS, designed for fans to financially support their favorite creators' projects. It empowers creators—developers, artists, and influencers—to bring their ideas to life with the backing of their community.</p>
+            </div></a>
+          <hr />
           <a href="https://github.com/jai-singh5/Spotify-Clone.git">
             <div className="card">
               <img src="spotify-clone.png" alt="" />
               <h2>Spotify Clone</h2>
               <p>Made a Project on music streaming application built using JavaScript, HTML, and CSS. It features a sleek, user-friendly interface that mimics the look and feel of Spotify. Users can browse and play tracks, view playlists, and enjoy a seamless listening experience.</p>
             </div> </a>
+          <hr />
+          <a href="https://github.com/jai-singh5/X-Twitter-.git">
+            <div className="card">
+              <img src="X.png" alt="" />
+              <h2>X(Twitter) - Clone</h2>
+              <p>This project is a simple clone of the X (formerly Twitter) UI using only HTML and Tailwind CSS. The goal is to replicate the main user interface of X while leveraging Tailwind CSS for utility-first styling. Features</p>
+            </div></a>
+          <hr />
+          <a href="https://github.com/jai-singh5/Snake-Game.git">
+            <div className="card">
+              <img src="Snake Game.png" alt="" />
+              <h2>Snake Game</h2>
+              <p>This classic Snake Game is built with JavaScript, HTML, and CSS. It features smooth gameplay and a responsive design, offering a nostalgic gaming experience. Players navigate the snake to collect food while avoiding collisions with the walls and the snake's own tail.</p>
+            </div></a>
+          <hr />
+          <a href="https://github.com/jai-singh5/Rock-Paper-Scissor.git">
+            <div className="card">
+              <img src="SPS game.png" alt="" />
+              <h2>Rock Paper Scissor</h2>
+              <p>This is a simple implementation of the classic Rock, Paper, Scissors game using HTML, CSS, and JavaScript. The user plays against the computer, and the game keeps track of the scores.</p>
+            </div></a>
+          <hr />
+          <a href="https://github.com/jai-singh5/Tic-Tac-Toe.git">
+            <div className="card">
+              <img src="Tic-Tac-Toe.png" alt="" />
+              <h2>Tic-Tac-Toe</h2>
+              <p>Welcome to the Tic-Tac-Toe game, a simple and fun project built using HTML, CSS, and JavaScript! This is the classic game where two players take turns marking spaces in a 3x3 grid, aiming to be the first to get three marks in a row (horizontally, vertically, or diagonally).</p>
+            </div></a>
+          <hr />
+          <a href="https://github.com/jai-singh5/Currency-Converter.git">
+            <div className="card">
+              <img src="Currency Converter.png" alt="" />
+              <h2>Currency Converter</h2>
+              <p>A simple, user-friendly web-based currency converter that allows users to convert between multiple currencies using real-time exchange rates fetched from the ExchangeRate-API.</p>
+            </div></a>
           <hr />
           <a href="https://github.com/jai-singh5/To-Do-List.git">
             <div className="card">
@@ -20,54 +62,13 @@ function Projects() {
               <p>This project is a simple yet functional To-Do List application developed using JavaScript, HTML, and CSS. It allows users to add, edit, and remove tasks with ease.The simple interface and responsive design ensure a smooth user experience.</p>
             </div></a>
           <hr />
-          <a href="https://github.com/jai-singh5/Snake-Game.git"><div className="card">
-            <img src="Snake Game.png" alt="" />
-            <h2>Snake Game</h2>
-            <p>This classic Snake Game is built with JavaScript, HTML, and CSS. It features smooth gameplay and a responsive design, offering a nostalgic gaming experience. Players navigate the snake to collect food while avoiding collisions with the walls and the snake's own tail.</p>
-          </div></a>
-          <hr />
           <a href="https://github.com/jai-singh5/Portfolio.git">
             <div className="card">
               <img src="Portfolio.png" alt="" />
               <h2>Portfolio</h2>
               <p>This portfolio website, built with React.js, showcases my projects, skills in a sleek and interactive format. With responsive design and layout, it highlights my expertise in web development. This site demonstrates my ability to create dynamic and professional web applications.</p>
             </div></a>
-            <hr />
-          <a href="https://github.com/jai-singh5/Fuel-My-Work.git">
-            <div className="card">
-              <img src="Fuel My Work!.png" alt="" />
-              <h2>Fuel My Work!</h2>
-              <p>Fuel My Work is a crowdfunding platform built with Next.js and Tailwind CSS, designed for fans to financially support their favorite creators' projects. It empowers creators—developers, artists, and influencers—to bring their ideas to life with the backing of their community.</p>
-            </div></a>
-            <hr />
-          <a href="https://github.com/jai-singh5/X-Twitter-.git">
-            <div className="card">
-              <img src="X.png" alt="" />
-              <h2>X(Twitter) - Clone</h2>
-              <p>This project is a simple clone of the X (formerly Twitter) UI using only HTML and Tailwind CSS. The goal is to replicate the main user interface of X while leveraging Tailwind CSS for utility-first styling. Features</p>
-            </div></a>
-            <hr />
-            <a href="https://github.com/jai-singh5/Currency-Converter.git">
-            <div className="card">
-              <img src="Currency Converter.png" alt="" />
-              <h2>Currency Converter</h2>
-              <p>A simple, user-friendly web-based currency converter that allows users to convert between multiple currencies using real-time exchange rates fetched from the ExchangeRate-API.</p>
-            </div></a>
-            <hr />
-            <a href="https://github.com/jai-singh5/Rock-Paper-Scissor.git">
-            <div className="card">
-              <img src="SPS game.png" alt="" />
-              <h2>Rock Paper Scissor</h2>
-              <p>This is a simple implementation of the classic Rock, Paper, Scissors game using HTML, CSS, and JavaScript. The user plays against the computer, and the game keeps track of the scores.</p>
-            </div></a>
-            <hr />
-            <a href="https://github.com/jai-singh5/Tic-Tac-Toe.git">
-            <div className="card">
-              <img src="Tic-Tac-Toe.png" alt="" />
-              <h2>Tic-Tac-Toe</h2>
-              <p>Welcome to the Tic-Tac-Toe game, a simple and fun project built using HTML, CSS, and JavaScript! This is the classic game where two players take turns marking spaces in a 3x3 grid, aiming to be the first to get three marks in a row (horizontally, vertically, or diagonally).</p>
-            </div></a>
-            <hr />
+          <hr />
         </div>
       </div>
     </>
